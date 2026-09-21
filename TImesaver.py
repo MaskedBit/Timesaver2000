fa4cb65885ecb6c1881da7ef46d64c1b7a617ef0
Steve Boylan <maskedbit@gmail.com>
2026-09-21 14:40:28 -0400

    Skeleton application

--- TImesaver.py
+++ TImesaver.py
@@ -1,14 +1,26 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Timesaver 2000")
+class Timesaver(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Timesaver 2000")
+        self.geometry('600x400')
+        tk.Label(self, text="Timesaver 2000").grid(row=0, column=0, columnspan=2)
+        self.timeDisplay = tk.StringVar(self, "00:00:00")
+        self.timeLabel = tk.Label(self, textvariable=self.timeDisplay).grid(row=1, column=0, columnspan=2)
+        self.buttonLabel = tk.StringVar(self, "Start")
+        tk.Button(self, textvariable=self.buttonLabel, command=self.start).grid(row=3, column=0)
+        tk.Button(self, text="Penalty", command=self.penalty).grid(row=3, column=1)
 
-def closer( event ):
-    root.destroy()
+    def start(self):
+        print("Start clicked")
 
-root.bind( "<Escape>", closer )
+    def stop(self):
+        print("Stop clicked")
 
-ctrl = tk.Toplevel()
-ctrl.title("Control window")
+    def penalty(self):
+        print("PENALTY!!")
 
-root.mainloop()
+if __name__ == "__main__":
+    app = Timesaver()
+    app.mainloop()
